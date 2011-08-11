51e194934f5c9a78038ba52222ae7777909ce0ff
Ryan Edgell <edgell@gmail.com>
2011-08-11 10:08:21 -0500

    refactor
    Updated to be more gooder

--- js/jquery.doublerainbow.js
+++ js/jquery.doublerainbow.js
@@ -1,36 +1,32 @@
-/*! Copyright (c) 2010 Ryan Edgell (http://www.ryanmarc.com/)
+/*! Copyright (c) 2011 Ryan Edgell (http://www.ryanmarc.com/)
 * Licensed under the MIT License (LICENSE.txt).
 *
-* Version 1.0
+* Version 2.0
 *
 * Based on the full on double rainbow, almost a triple rainbow. As seen here: http://www.youtube.com/watch?v=OQSNhk5ICTI
 * OMG IT'S SO BRIGHT AND VIVID!
 */
 
 (function($){
-
-$.fn.doubleRainbow = function() {
-	this.each( function() {
-		var $r_top = parseInt($(this).position().top) + 
-					 parseInt($(this).css('padding-top').replace('px','')) + 
-					 parseInt($(this).css('margin-top').replace('px',''));
-		
-		var $r_left = parseInt($(this).position().left) + 
-					  parseInt($(this).css('padding-left').replace('px','')) + 
-					  parseInt($(this).css('margin-left').replace('px',''));
-		
-		var $r_width = parseInt($(this).css('width').replace('px',''));
-		var $r_height = parseInt($(this).css('height').replace('px',''));
-
-		$('<img src="images/rainbow.gif" alt="rainbow" />').insertAfter(this);
-		
-		$r = $(this).next();
-		$r.css('position', 'absolute')
-			.css('top', $r_top)
-			.css('left', $r_left)
-			.width($r_width)
-			.height($r_height);
-	});
-};
-
+	$.fn.doubleRainbow = function() {
+		this.each( function() {
+			$(this)
+				.wrap($("<div />", {
+					css			: { 
+						'position' 	: 'relative',
+						'width'		: $(this).width(),
+						'height'	: $(this).height()
+					}
+				}))
+				.after('<img src="../images/rainbow.gif" alt="rainbow" />')
+				.next()
+				.css({
+					'position'	: 'absolute',
+					'top' 		: 0,
+					'left'		: 0,
+					'width'		: $(this).width(),
+					'height'	: $(this).height()
+				});
+		});
+	};
 })(jQuery);
